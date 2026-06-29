74acc718657a29b377cb3452176d69d778a9de91
vertexacadamy <vertexacademy221@gmail.com>
2026-06-29 23:15:15 +0000

    Delete 02_ndk/app/.cxx directory

--- 02_ndk/app/.cxx/Debug/o5p395h2/arm64-v8a/third-party/openal/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bullydd/Documents/Android/vertex-academy/Masteringndk/02_ndk/app/src/main/cpp/third-party/openal
-# Build directory: /home/bullydd/Documents/Android/vertex-academy/Masteringndk/02_ndk/app/.cxx/Debug/o5p395h2/arm64-v8a/third-party/openal
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
