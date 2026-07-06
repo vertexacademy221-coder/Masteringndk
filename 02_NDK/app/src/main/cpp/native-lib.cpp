6a066b0f08730d1ca2a38a215984f339acbf4a6d
vertexacademy221-coder <vertexacademy221@gmail.com>
2026-07-06 10:27:24 +0000

    add freetype, st_image dependencies

--- 02_NDK/app/src/main/cpp/native-lib.cpp
+++ 02_NDK/app/src/main/cpp/native-lib.cpp
@@ -7,6 +7,12 @@
 #include <cmath>
 #include <vector>
 
+extern "C"
+{
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+}
+
 #define LOG_TAG "NDK-AudioEngine"
 #define LOGI(...)__android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 #define LOGE(...)__android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
@@ -58,7 +64,7 @@ Java_com_vertexacademy_masteringndk_AudioEngine_initOpenAL(JNIEnv *env, jobject 
 {
     // Init CURL
     initCurl();
-
+    
     // 1. Ouvrir le peripherique audio par defaut d'android
     openal_device = alcOpenDevice(nullptr);
     if (!openal_device){
